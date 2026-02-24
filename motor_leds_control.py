5cc96a78970d99dc06ec569c76d4963a56973682
Vietanh09876 <nguyenvietanh09876@gmail.com>
2026-02-24 11:37:38 +0000

    atu logo

--- motor_leds_control.py
+++ motor_leds_control.py
@@ -1,8 +1,11 @@
-import RPi.GPIO as GPIO
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
 from gpiozero import Motor
 import spidev
 import time
 import datetime
+import json_handler
 
 #Motor config
 motor_1 = Motor(forward=2, backward=3)
@@ -14,26 +17,26 @@ spi.max_speed_hz = 500000 #refer to minimum clock high duration
 spi.mode = 0
 leds = 0b0000 #LEDS setting read from right to left
 leds_off = 0b0000 #Turn off all leds
-
 print(format(leds, "04b"))
-
 spi.xfer2([leds])
-# time.sleep(2)
-# leds = leds_off
-# spi.xfer2([leds_off])
-# 
-# time.sleep(2)
-# leds |= (1 << 3)
-# spi.xfer([leds])
-# 
-# time.sleep(2)
-# leds ^= (1 << 3)
-# spi.xfer2([leds])
 
+def checkmoisture():
+    return
 
+def configHMI():
+    #Create plain gui
+    gui = tk.Tk()
+    gui.title("Gui demo")
+
+    #Config window size and bg colour
+    tools_frame = tk.Frame(gui, width=200, height=400, bg="skyblue")
+    tools_frame.grid(row=0, column=0)
+    
+    
 # while True:
 #     motor_1.forward(speed=1)
 #     time.sleep(2)
 #     motor_1.stop()
 #     print("ok")
+
     
