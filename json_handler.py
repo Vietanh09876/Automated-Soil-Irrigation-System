130d6bb2b8ffb5b0e30d69b0365708429890db6d
Vietanh09876 <nguyenvietanh09876@gmail.com>
2026-02-25 16:06:15 +0000

    changing

--- json_handler.py
+++ json_handler.py
@@ -1,10 +1,11 @@
 import json
 
-def writejson_moisture(dictionary: dict = {"field 1": 100, "field 2": 100}):
+def writejson_moisture(dictionary: dict = {"field 1": 100, "field 2": 100}, time: str = "00:00:01"):
 
     with open("data/field_data.json", "w") as file:
         data = {
-            "field moisture": dictionary
+            "field moisture": dictionary,
+            "timestamp": time
         }
 
         json.dump(data, file, indent=2, sort_keys=True)
@@ -18,6 +19,6 @@ def readjson_moisture():
         
     return dictionary
 
-# writejson_moisture({"field 1": 100, "field 2": 200, "field 3": 300})
+writejson_moisture({"field 1": 100, "field 2": 200, "field 3": 300})
 
 # print(readjson_moisture())
